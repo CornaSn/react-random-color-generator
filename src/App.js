c80365683db7696a7ed2fcf008daf11ce449eb61
CornaSn <cornelia.schenk@hotmail.com>
2024-05-14 22:43:35 +0200

    Add code for color generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import './App.css';
-import logo from './logo.svg';
+import { randomColor } from 'randomcolor';
+import { useState } from 'react';
 
 export default function App() {
+  const color = randomColor();
+  // Create a state for the button
+  const [hexCode, setHexCode] = useState(color);
+  console.log(hexCode);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        <h1>Random Color Generator</h1>
+      </div>
+
+      <div>
+        <button
+          style={{
+            fontSize: '14px',
+            textAlign: 'center',
+            padding: '6px',
+            margin: '20px',
+            border: '1px solid black',
+            position: 'absolute',
+          }}
+          onClick={() => {
+            const newColor = randomColor();
+            setHexCode(newColor);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Generate Random Color
+        </button>
+      </div>
+      <div
+        style={{
+          backgroundColor: `${hexCode}`,
+          textAlign: 'center',
+          fontSize: '30px',
+          margin: '100px',
+          height: '200px',
+          padding: '30px 70px',
+        }}
+      >
+        {' '}
+        Generated Color:
+        {hexCode}
+      </div>
+    </>
   );
 }
