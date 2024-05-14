5576b3b0ae79971d49785bbfe4dabe7c7c31af2b
CornaSn <cornelia.schenk@hotmail.com>
2024-05-15 00:39:04 +0200

    Add Hue & Luminosity code

--- src/App.js
+++ src/App.js
@@ -1,50 +1,82 @@
 import { randomColor } from 'randomcolor';
 import { useState } from 'react';
-import styled from 'styled-components';
 
 export default function App() {
   const color = randomColor();
   // Create a state for the button
   const [hexCode, setHexCode] = useState(color);
-  console.log(hexCode);
+  const [hue, setColorHue] = useState('Enter a hue here...');
+  const [lum, setColorLightness] = useState('Enter a luminosity here...');
+  const param = {
+    hue: hue,
+    luminosity: lum,
+  };
 
   return (
     <>
-      <div>
-        <h1
-          style={{
-            backgroundColor: 'black',
-            color: `${hexCode}`,
-            textAlign: 'center',
-          }}
-        >
-          Random Color Generator
-        </h1>
-      </div>
+      <h1
+        style={{
+          backgroundColor: 'black',
+          color: `${hexCode}`,
+          textAlign: 'center',
+        }}
+      >
+        Random Color Generator
+      </h1>
+      <input
+        style={{
+          border: '2px solid #ccc',
+          borderRadius: '10%',
+          padding: '10px',
+          width: '300px',
+          height: '30px',
+          fontSize: '16px',
+          backgroundColor: '#f0f0f0',
+          color: '#333',
+          boxSizing: 'border-box',
+          marginLeft: '300px',
+        }}
+        value={hue}
+        onChange={(event) => {
+          setColorHue(event.currentTarget.value);
+        }}
+      />
+      <input
+        style={{
+          border: '2px solid #ccc',
+          borderRadius: '10%',
+          padding: '10px',
+          width: '300px',
+          height: '30px',
+          fontSize: '16px',
+          backgroundColor: '#f0f0f0',
+          color: '#333',
+          boxSizing: 'border-box',
+        }}
+        value={lum}
+        onChange={(event) => setColorLightness(event.currentTarget.value)}
+      />
+      <button
+        style={{
+          border: '2px solid #ccc',
+          borderRadius: '50%',
+          padding: '20px',
+          width: '150px',
+          height: '150px',
+          fontSize: '16px',
+          backgroundColor: '#f0f0f0',
+          boxSizing: 'border-box',
+          marginLeft: '200px',
+          color: `${hexCode}`,
+        }}
+        onClick={() => {
+          const newColor = randomColor(param);
+          setHexCode(newColor);
+        }}
+      >
+        Generate Random Color
+      </button>
 
-      <div>
-        <button
-          style={{
-            fontSize: '14px',
-            textAlign: 'center',
-            padding: '12px',
-            marginLeft: '50px',
-            marginTop: '60px',
-            height: '300px',
-            border: '5px solid black',
-            borderRadius: '30px',
-            position: 'absolute',
-            backgroundColor: 'black',
-            color: `${hexCode}`,
-          }}
-          onClick={() => {
-            const newColor = randomColor();
-            setHexCode(newColor);
-          }}
-        >
-          Generate Random Color
-        </button>
-      </div>
       <div
         style={{
           backgroundColor: `${hexCode}`,
@@ -52,8 +84,8 @@ export default function App() {
           borderRadius: '3rem',
           textAlign: 'center',
           fontSize: '30px',
-          margin: '30px',
-          height: '20px',
+          margin: '3px',
+          height: '10px',
           padding: '200px',
         }}
       >
